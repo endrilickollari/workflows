24118d44e09af5f54a08974111856d5063962142
_endrilickollari <endri.lickollari@ltadvisors.it>
2025-04-11 15:53:14 +0200

    add better auth config

---
+++ src/libs/auth/schema.ts
@@ -0,0 +1,53 @@
+import {integer, sqliteTable, text} from "drizzle-orm/sqlite-core";
+import {sql} from "drizzle-orm";
+
+export const users = sqliteTable('ba_users', {
+    id: text("id").primaryKey(),
+    email: text("email").notNull().unique(),
+    emailVerified: integer("email_verified", { mode: "boolean" }).default(false),
+    password: text("password"),
+    firstName: text("first_name"),
+    lastName: text("last_name"),
+    plan: text("plan").default("Free"),
+    isAdmin: integer("is_admin", { mode: "boolean" }).default(false),
+    image: text("image"),
+    createdAt: text("created_at").default(sql`CURRENT_TIMESTAMP`),
+    updatedAt: text("updated_at").default(sql`CURRENT_TIMESTAMP`),
+});
+
+export const sessions = sqliteTable("ba_sessions", {
+    id: text("id").primaryKey(),
+    userId: text("user_id").notNull(),
+    expiresAt: integer("expires_at").notNull(),
+    createdAt: text("created_at").default(sql`CURRENT_TIMESTAMP`),
+});
+
+export const accounts = sqliteTable("ba_accounts", {
+    id: text("id").primaryKey(),
+    userId: text("user_id").notNull(),
+    provider: text("provider").notNull(),
+    providerAccountId: text("provider_account_id").notNull(),
+    refreshToken: text("refresh_token"),
+    accessToken: text("access_token"),
+    expiresAt: integer("expires_at"),
+    tokenType: text("token_type"),
+    scope: text("scope"),
+    idToken: text("id_token"),
+    createdAt: text("created_at").default(sql`CURRENT_TIMESTAMP`),
+    updatedAt: text("updated_at").default(sql`CURRENT_TIMESTAMP`),
+});
+
+export const verifications = sqliteTable("ba_verifications", {
+    id: text("id").primaryKey(),
+    userId: text("user_id"),
+    token: text("token").notNull(),
+    expires: integer("expires").notNull(),
+    identifier: text("identifier").notNull(),
+    createdAt: text("created_at").default(sql`CURRENT_TIMESTAMP`),
+});
+
+export type User = typeof users.$inferSelect;
+export type NewUser = typeof users.$inferInsert;
+export type Session = typeof sessions.$inferSelect;
+export type Account = typeof accounts.$inferSelect;
+export type Verification = typeof verifications.$inferSelect;
