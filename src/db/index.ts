cea600fae8f246bb6160631dcd4c31b8d12ead9c
_endrilickollari <endri.lickollari@ltadvisors.it>
2025-04-15 23:00:31 +0200

    change db secret

--- src/db/index.ts
+++ src/db/index.ts
@@ -4,7 +4,7 @@ import { users, type User, type NewUser } from '../libs/auth/schema';
 
 // Create Postgres client with neon.tech serverless postgres
 const connectionString = process.env.DATABASE_URL || 
-  'postgresql://neondb_owner:npg_SFgdyKq1mk3x@ep-crimson-rice-a585j8zw-pooler.us-east-2.aws.neon.tech/workflows_local?sslmode=require';
+  '';
 
 const client = postgres(connectionString);
 export const db = drizzle(client);
